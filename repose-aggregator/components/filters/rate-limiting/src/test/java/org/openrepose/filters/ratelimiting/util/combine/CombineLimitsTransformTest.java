b86e149c9830dca8150e902c608eaf93498099bc
David Kowis <david.kowis@rackspace.com>
2015-02-23 14:14:22 -0600

    Don't use REGEXP on XML, it's brittle
    http://stackoverflow.com/a/1732454/423218
    This fixes it for now, but it's not reliable.

--- repose-aggregator/components/filters/rate-limiting/src/test/java/org/openrepose/filters/ratelimiting/util/combine/CombineLimitsTransformTest.java
+++ repose-aggregator/components/filters/rate-limiting/src/test/java/org/openrepose/filters/ratelimiting/util/combine/CombineLimitsTransformTest.java
@@ -24,7 +24,10 @@ import java.util.regex.Pattern;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
-//TODO: this test fails sometimes!
+/**
+ * TODO: seems that sometimes this test includes namespaces on some of the elements.
+ * Not while being run in the IDe, but sometimes in maven, and almost every time with gradle.
+ */
 public class CombineLimitsTransformTest {
 
     public static final String SIMPLE_URI_REGEX = "/loadbalancer/.*";
@@ -37,7 +40,12 @@ public class CombineLimitsTransformTest {
     public static final String COMBINER_XSL_LOCATION = "/META-INF/xslt/limits-combine.xsl";
     public static final ObjectFactory LIMITS_OBJECT_FACTORY = new ObjectFactory();
 
-    private final Pattern validationPattern = Pattern.compile(".*(<rates xmlns.*>.*</rates>).*(<absolute>.*</absolute>).*", Pattern.DOTALL);
+    //This validation pattern needs to take into account that there might be an xml namespace in there.
+    //Seems that the namespace gets dropped on the <rates> tag
+    //TODO: a better solution to this would be to xpath out the elements and ensure that the xpath structure matches
+    //The below regex is brittle and I just did to get it passing again.
+    //Don't use regexp on XML: http://stackoverflow.com/a/1732454/423218
+    private final Pattern validationPattern = Pattern.compile(".*(<[\\S:]*rates xmlns.*>.*</[\\S:]*rates>).*(<absolute>.*</absolute>).*", Pattern.DOTALL);
     private final Pattern validationPatternJson = Pattern.compile(".*\"rate\":.*(\"absolute\":).*", Pattern.DOTALL);
     private StreamTransform<LimitsTransformPair, OutputStream> combiner;
 
